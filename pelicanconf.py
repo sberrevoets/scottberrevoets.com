6f9e8cb9e781fa2ec9027873cda47facad620e17
Scott Berrevoets <s.berrevoets@me.com>
2016-04-10 15:08:42 -0700

    Adjust settings so everything is just right
    This includes proper URLs, ordering in the menu, and exclusion of files I don't
    care about.

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,21 +2,39 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-ARCHIVES_SAVE_AS='archive.html'
+# Site information
+AUTHOR = 'Scott Berrevoets'
+SITENAME = 'Scott Berrevoets'
+SITEURL = 'http://scottberrevoets.com'
 
-AUTHOR = u'Scott Berrevoets'
-SITENAME = u'Scott Berrevoets'
-SITEURL = ''
+# Show line numbers in code snippets
+MD_EXTENSIONS = ['codehilite(css_class=highlight, linenums = True)', 'extra']
 
+# Path specifications
+RELATIVE_URLS = False
 PATH = 'content'
 THEME = './theme'
 
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+PAGE_URL = '{slug}.html'
+PAGE_SAVE_AS = '{slug}.html'
+
+# Create and link to index and archive
+DIRECT_TEMPLATES = ('index', 'archive')
+MENUITEMS = [('Blog', '/'), ('Archive', '/archive.html')]
+
+# Disable authors, categories, and tags
+AUTHOR_SAVE_AS = False
+CATEGORY_SAVE_AS = False
+TAG_SAVE_AS = False
+
+# Locale settings
+DEFAULT_LANG = 'en'
 TIMEZONE = 'America/Los_Angeles'
 DATE_FORMATS = { 'en': '%d %B %Y' }
 
-DEFAULT_LANG = u'en'
-
-DISPLAY_PAGES_ON_MENU = True
 DISPLAY_SOCIAL_MEDIA = True
 SOCIALMEDIAITEMS = [('twitter.svg', 'https://twitter.com/ScottBerrevoets'),
                     ('github.svg', 'https://github.com/sberrevoets'),
@@ -30,18 +48,6 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = 5
 
-MD_EXTENSIONS = ['codehilite(css_class=highlight, linenums = True)', 'extra']
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
