a11d6fcec633ca48357c738f69024364477454f8
Scott Berrevoets <s.berrevoets@me.com>
2016-04-13 18:46:39 -0700

    Add Google analytics key

--- publishconf.py
+++ publishconf.py
@@ -20,9 +20,8 @@ FEED_ALL_ATOM = 'feeds/all.atom.xml'
 FEED_ALL_RSS = 'feeds/all.rss.xml'
 
 # Following items are often useful when publishing
-
 DISQUS_SITENAME = 'sberrevoets'
-#GOOGLE_ANALYTICS = ""
+GOOGLE_ANALYTICS = "UA-53307558-1"
 
 # Clean up
 DELETE_OUTPUT_DIRECTORY = True
